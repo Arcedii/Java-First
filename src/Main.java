e3ac16c62a07328899ddfb88933b47f82a3a0ff8
Arcedii <91275440+Arcedii@users.noreply.github.com>
2023-03-30 21:11:13 +0300

    first-commit

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,8 @@ package src;
 public class Main {
     public static void main(String[] args)
     {
-        //какая-то хуйня
-        System.out.println("Hello\\\"\nWorld\t!");
-
-        /* большой коментарий
-
-
-           шоб не забыть что там да как
-         */
-        System.out.println("\nКонец\n\n\n\n\tНо не мой");
+       int i;
+       i = 1;
+       System.out.println(i);
     }
 }
