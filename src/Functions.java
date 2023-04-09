4fe05c94dd3abf40629268313003f5ac87456bd3
Arcedii <91275440+Arcedii@users.noreply.github.com>
2023-04-09 19:49:55 +0300

    Collection(Lists)

---
+++ src/Functions.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Functions {
+    public static void main(String[] args) {
+         summa(2, 3);
+    }
+    public  static void summa(int a, int b) {
+        int result = a + b;
+        System.out.println(result);
+    }
+}
